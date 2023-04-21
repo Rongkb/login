d442e12cdda0f574603db0d562dca117c15720a4
Bui Trong <vantrongnshb@gmail.com>
2023-04-21 17:22:20 +0700

    them bootstrap

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,16 +1,20 @@
-$.ajax({
-    url: './api/user?page=3',
-    type: "GET"
-})
-    .then(data => {
-        console.log(data)
-        for (i = 0; i < data.length; i++) {
-            const element = data[i]
-            var item = $(`<h1>${element.username} : ${element.password}</h1>`)
-            $('#content').append(item)
 
-        }
+
+function loadPage(page) {
+    $.ajax({
+        url: './api/user?page=' + page,
+        type: "GET"
     })
-    .catch(err => {
-        console.log(err)
-    })
+        .then(data => {
+            console.log(data)
+            for (i = 0; i < data.length; i++) {
+                const element = data[i]
+                var item = $(`<h1>${element.username} : ${element.password}</h1>`)
+                $('#content').append(item)
+
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
