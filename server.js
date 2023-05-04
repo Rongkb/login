0fa2b065710c0c2e347f1eccba2a402ac42d0562
Bui Trong <vantrongnshb@gmail.com>
2023-05-04 17:43:57 +0700

    trien khai passport

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ var { login, generateToken, updateRefreshToken } = require('./src/middleware/log
 var check_login = require('./src/middleware/check_login')
 var jwt = require('jsonwebtoken')
 var cookieParser = require('cookie-parser')
-// var passport = require('passport')
-// var LocalStrategy = require('passport-local').Strategy;
+var passport = require('passport');
+var expressSession = require('express-session');
+var LocalStrategy = require('passport-local').Strategy;
 
 require('dotenv').config()
 const port = process.env.PORT || 8081
@@ -19,7 +20,7 @@ const { AcountModel } = require('./src/model/account');
 
 
 app.use('/static', express.static(path.join(__dirname, 'src/public')))
-
+// cors
 app.use((req, res, next) => {
     res.header(`Access-Control-Allow-Origin`, `*`);
     res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
@@ -30,8 +31,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
-
 
 app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, './src/view/home.html'))
@@ -89,8 +88,152 @@ app.get('/private', check_login, (req, res, next) => {
     })
 })
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+
+app.use(expressSession({ secret: 'mySecretKey' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post("/passport", passport.authenticate('local', {
+    failureRedirect: "/login",
+    successRedirect: '/'
+}));
+
+passport.use(new LocalStrategy((username, password, done) => {
+    var user = AcountModel.findOne({ username: username })
+    if (user) {
+        return done(null, user)
+    } else {
+        return done(null, false)
+    }
+}
+))
+
+
+// Mã hoá và giải mã những giá trị của người dùng
+passport.serializeUser(function (user, done) {
+    done(null, user._id);
+});
+
+
+// passport.deserializeUser(function (id, done) {
+//     User.findById(id, function (err, user) {
+//         done(err, user);
+//     });
+// });
+
+// //   Chiến lược login
+// // passport/login.js 
+// passport.use('login', new LocalStrategy({
+//     passReqToCallback: true
+// },
+//     function (req, username, password, done) {
+//         // check in mongo if a user with username exists or not 
+//         User.findOne({ 'username': username },
+//             function (err, user) {
+//                 // In case of any error, return using the done method 
+//                 if (err)
+//                     return done(err);
+//                 // Username does not exist, log error & redirect back 
+//                 if (!user) {
+//                     console.log('User Not Found with username ' + username);
+//                     return done(null, false,
+//                         req.flash('message', 'User Not found.'));
+//                 }
+//                 // User exists but wrong password, log the error 
+//                 if (!isValidPassword(user, password)) {
+//                     console.log('Invalid Password');
+//                     return done(null, false,
+//                         req.flash('message', 'Invalid Password'));
+//                 }
+//                 // User and password both match, return user from 
+//                 // done method which will be treated like success 
+//                 return done(null, user);
+//             }
+//         );
+//     }));
+
+// // Chiến lược đăng ký
+// passport.use('signup', new LocalStrategy({
+//     passReqToCallback: true
+// })),
+//     function (req, username, password, done) {
+//         findOrCreateUser = function () {
+//             // find a user in Mongo with provided username 
+//             User.findOne({ 'username': username }, function (err, user) {
+//                 // In case of any error return 
+//                 if (err) {
+//                     console.log('Error in SignUp: ' + err);
+//                     return done(err);
+//                 }
+//                 // already exists 
+//                 if (user) {
+//                     console.log('User already exists');
+//                     return done(null, false,
+//                         req.flash('message', 'User Already Exists'));
+//                 } else {
+//                     // if there is no user with that email 
+//                     // create the user 
+//                     var newUser = new User();
+//                     // set the user's local credentials 
+//                     newUser.username = username;
+//                     newUser.password = createHash(password);
+//                     newUser.email = req.param('email');
+//                     newUser.firstName = req.param('firstName');
+//                     newUser.lastName = req.param('lastName');
+//                     // save the user 
+//                     newUser.save(function (err) {
+//                         if (err) {
+//                             console.log('Error in Saving user: ' + err);
+//                             throw err;
+//                         }
+//                         console.log('User Registration succesful');
+//                         return done(null, newUser);
+//                     });
+//                 }
+//             });
+//         }
+//     };
+
+// // Delay the execution of findOrCreateUser and execute 
+// // the method in the next tick of the event loop 
+// process.nextTick(findOrCreateUser);
+
+
+
+// app.post("/passport", passport.authenticate('local', {
+//     failureRedirect: "/login",
+//     successRedirect: '/'
+// }));
+
+// let initPassportLocal = () => {
+//     passport.use(new LocalStrategy({
+
+//     }, async (req, email, password, done) => {
+//         try {
+//             let user = await UserModel.findByEmail(email);
+//             if (!user) {
+//                 return done(null, false);
+//             }
+
+//             let checkPassword = await (user.comparePassword(password));
+
+//             if (!checkPassword) {
+//                 return done(null, false);
+//             }
+
+//             return done(null, user);
+//         } catch (error) {
+//             console.log(error);
+//             return done(null, false,);
+//         }
+//     }));
+// };
+
+
+
+
+
+
 
 app.use('/user/', routerUser)
 app.use('/api/', router)
